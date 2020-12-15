40d954531d3fabb253fc99a21293823ed63d3193
RomanRusanov <roman9628@gmail.com>
2020-12-15 17:14:55 +0300

    create job4j_cinema

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+-- CREATE TABLE post (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT
+-- );
+-- CREATE TABLE candidate (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT,
+--     photo_id NUMERIC,
+--     city_id NUMERIC
+-- );
+-- CREATE TABLE photo (
+--     id SERIAL PRIMARY KEY,
+--     path TEXT
+-- );
+-- CREATE TABLE userapp (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT,
+--     email TEXT,
+--     password TEXT
+-- );
+-- CREATE TABLE city (
+--    id SERIAL PRIMARY KEY,
+--    name TEXT
+-- );
